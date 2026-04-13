89738b867138508a593fc4cf42cb27cbdc583611
Artem <swg.ignore@email.com>
2026-04-13 22:00:33 +0300

    open cors

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT ?? 3001;
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/habits", habitsRouter);
